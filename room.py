5b03cd0f075cebc8c25efb3aaaf9d7dc9b5197f2
jamorrison <jamorrison@users.noreply.github.com>
2023-03-22 14:27:18 -0400

    round numbers, add adjust function, and some refactoring

--- room.py
+++ room.py
@@ -10,14 +10,28 @@ class Room:
     def heat_gain(self, heater):
         """Rate of thermal energy gained by the room."""
         if heater.on:
-            return utils.HEATER_AIR_MASS * utils.AIR_SPECIFIC_HEAT * (utils.HEATER_TEMP - self.temp)
+            return round(
+                utils.HEATER_AIR_MASS * utils.AIR_SPECIFIC_HEAT * (utils.HEATER_TEMP - self.temp),
+                2
+            )
         else:
             return 0
 
-    def heat_loss(self, config, outside_temp):
+    def heat_loss(self, conf, outside_temp):
         """Rate of thermal energy lost from room to outside."""
-        return (self.temp - outside_temp) / config['R_effective']
+        return round(
+            (self.temp - outside_temp) / conf['R_effective'],
+            2
+        )
 
-    def heat_change(self, config, heater, outside_temp):
-        return (heat_gain(heater) - heat_loss(config, outside_temp)) / (config['air_mass'] * utils.AIR_SPECIFIC_HEAT)
+    def heat_change(self, conf, heater, outside_temp):
+        gain = self.heat_gain(heater)
+        loss = self.heat_loss(conf, outside_temp)
+        return round(
+            (gain - loss) / (conf['air_mass'] * utils.AIR_SPECIFIC_HEAT),
+            2
+        )
+
+    def add_delta_temp(self, delta):
+        self.temp = round(self.temp + delta, 2)
 
