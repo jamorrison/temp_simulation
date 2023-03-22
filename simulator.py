8d59e40f9c00f87115b40b59f76e4ef060fb1fea
jamorrison <jamorrison@users.noreply.github.com>
2023-03-22 14:27:59 -0400

    some more work on the simulation

--- simulator.py
+++ simulator.py
@@ -1,11 +1,13 @@
+import matplotlib.pyplot as plt
 import json
+import math
 import sys
 
 import validate
-import heater
+from heater import Heater
 import errors
 import utils
-import room
+from room import Room
 
 def load_config():
     """Load config JSON file."""
@@ -71,33 +73,79 @@ def preprocess_values(d):
     # Effective R value
     d['R_effective'] = (R_wall * R_window) / (R_wall + R_window)
 
-    ## Starting temperature of room
+    ## Temperatures
+    # Starting room temperature
     d['room']['starting_temp'] = 22
+
+    # Average sinusoidal temperature
+    d['outside']['avg'] = (d['outside']['max_temp'] + d['outside']['min_temp']) / 2
+    d['outside']['amp'] = d['outside']['max_temp'] - d['outside']['avg']
 
     ## Simulation run time
     d['run_time'] = 2 * 24 * 60 # Number of minutes in 2 days
 
     return d
 
+def set_outdoor_temps(config):
+    """Set outdoor temperatures for each minute of simulation.
+
+    Inputs -
+        config - input values for simulation
+    Returns -
+        list
+    """
+    temps = []
+
+    if config['outside']['type'] == 'fixed':
+        temps = config['run_time'] * [config['outside']['fixed_temp']]
+    else:
+        for i in range(config['run_time']):
+            temps.append(
+                config['outside']['amp'] * math.cos(2*math.pi*i/1440 - 300) + config['outside']['avg']
+            )
+
+    return temps
+
 def main():
     # Load user defined values
     config = load_config()
 
     # Process user values, convert non-metric values if needed
-    input_values = preprocess_values(config)
+    inputs = preprocess_values(config)
 
     # TODO: start implementing the actual heat transfer
     # Set up room and it's initial conditions
-    room = room.Room(input_values['room']['starting_temp'])
+    room = Room(inputs['room']['starting_temp'])
 
     # Set up heater and determine if it should be on or off
-    heater = heater.Heater(input_values['room']['thermostat_temp'], False)
+    heater = Heater(inputs['room']['thermostat_temp'], False)
     heater.set_status(room.temp)
 
-    # Setup outside temperatures for each minute of the simulation
-    temps = []
-    if input_values['outside']['type'] == 'fixed':
-        temps = input_values['run_time'] * [input_values['outside']['fixed_temp']]
+    # Outdoor temperatures
+    o_temps = set_outdoor_temps(inputs)
+
+    # Run simulation
+    times   = list(range(inputs['run_time']))
+    r_temps = []
+    h_stats = []
+    for i in times:
+        # Set/check status of heater
+        heater.set_status(room.temp)
+
+        # Calculate heat gain/loss and adjust room temp
+        delta = room.heat_change(inputs, heater, o_temps[i])
+        room.add_delta_temp(delta)
+
+        r_temps.append(room.temp)
+        h_stats.append(1 if heater.on else 0)
+
+    #fig = plt.figure()
+
+    plt.plot(times, o_temps, 'k-', label='Outdoor Temp')
+    plt.plot(times, r_temps, 'r-', label='Room Temp')
+    plt.plot(times, h_stats, 'g-', label='Heater status')
+
+    plt.show()
 
     return None
 
